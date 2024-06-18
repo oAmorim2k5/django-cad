16265671fcb38eb7dcf35a1daf18c9804c6285ad
oAmorim2k5 <victorhuski@gmail.com>
2024-06-18 16:43:42 -0300

    front and data ending

---
+++ ecomDjango/projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import Usuario
+
+
+def home(request):
+    return render(request, 'usuarios/home.html')
+
+# Salvar os dados da tela para o banco de dados
+def usuarios(request):
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+    #Exibir todos os usuários já cadastrados em uma nova página
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+
+
+    # Retornar os dados para a pagina de listagem de usuários
+    return render(request, 'usuarios/usuarios.html', usuarios)
--- ecomDjango/projeto_cad_usuarios/projeto_cad_usuarios/asgi.py
+++ ecomDjango/projeto_cad_usuarios/projeto_cad_usuarios/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ecomDjango project.
+ASGI config for projeto_cad_usuarios project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecomDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_cad_usuarios.settings')
 
 application = get_asgi_application()
