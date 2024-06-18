cc76e829745b022ade983c6591cee89ec58b8b90
oAmorim2k5 <victorhuski@gmail.com>
2024-06-18 10:25:06 -0300

    django create

--- ecomDjango/manage.py
+++ ecomDjango/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecoDjango.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecomDjango.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
